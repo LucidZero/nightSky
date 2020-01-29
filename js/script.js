5f42cc51beb95363cf7dc837e8aebdbcb7552d60
LucidZero <sweetsare42@gmail.com>
2020-01-30 00:27:33 +0100

    maxlenght

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
-    
+    $(function() {
+        max_length = $("#fname").attr("maxlength");
+        $("#fname").attr("onkeypress", "if(this.value.length >= max_length) return false;");
+    });
+    $(function() {
+        max_length = $("#lname").attr("maxlength");
+        $("#lname").attr("onkeypress", "if(this.value.length >= max_length) return false;");
+    });
+    $(function() {
+        max_length = $("#msg").attr("maxlength");
+        $("#msg").attr("onkeypress", "if(this.value.length >= max_length) return false;");
+    });
 });
 
 
