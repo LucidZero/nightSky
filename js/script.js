70c77fa2d8927cc4c742a08de777223dedc1996f
LucidZero <sweetsare42@gmail.com>
2020-01-30 00:45:31 +0100

    trying to fix max lenght on android

--- js/script.js
+++ js/script.js
@@ -1,16 +1,27 @@
 $(document).ready(function(){
     $(function() {
-        max_length = $("#fname").attr("maxlength");
-        $("#fname").attr("onkeypress", "if(this.value.length >= max_length) return false;");
-    });
-    $(function() {
-        max_length = $("#lname").attr("maxlength");
-        $("#lname").attr("onkeypress", "if(this.value.length >= max_length) return false;");
-    });
-    $(function() {
-        max_length = $("#msg").attr("maxlength");
-        $("#msg").attr("onkeypress", "if(this.value.length >= max_length) return false;");
+    max_length = $("#fname").attr("maxlength");
+    $("#fname").on("keyup", function() {
+        if(this.value.length >= max_length) {
+            $(this).val($(this).val().substr(0, max_length));
+        }
     });
 });
-
+    $(function() {
+    max_length = $("#lname").attr("maxlength");
+    $("#lname").on("keyup", function() {
+        if(this.value.length >= max_length) {
+            $(this).val($(this).val().substr(0, max_length));
+        }
+    });
+});
+    $(function() {
+    max_length = $("#msg").attr("maxlength");
+    $("#msg").on("keyup", function() {
+        if(this.value.length >= max_length) {
+            $(this).val($(this).val().substr(0, max_length));
+        }
+    });
+});
+});
 
