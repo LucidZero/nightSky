ef048dff33476365e41ea04a5f455a1e0061d7b0
LucidZero <sweetsare42@gmail.com>
2020-02-09 15:25:20 +0100

    java

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
     $(function() {
-    maxLength = $("#fname").attr("maxlength");
-    $("#fname").on("keyup", function() {
+    maxLength = $("#firstName").attr("maxlength");
+    $("#firstName").on("keyup", function() {
         if(this.value.length >= maxLength) {
             $(this).val($(this).val().substr(0, maxLength));
         }
     });
 });
     $(function() {
-    maxLength = $("#lname").attr("maxlength");
-    $("#lname").on("keyup", function() {
+    maxLength = $("#lastName").attr("maxlength");
+    $("#lastName").on("keyup", function() {
         if(this.value.length >= maxLength) {
             $(this).val($(this).val().substr(0, maxLength));
         }
@@ -23,31 +23,15 @@ $(document).ready(function(){
         }
     });
 });
-});
-//^^^^^^^^^^^^^ this above is used to make form only go up to maxlength on phones
 
-//vvvvvv gallery jquery
-/*
-$(document).ready(function(){
-        var cw = $('#gallery').width();
-        console.log(cw);
-        
-        if(cw<=280){
-
-            $('.frame').css({'width':cw-'10p'});
-        }
+$(function(){
+    $("#firstName").text().charAt(0). toUpperCase();
+    
+    $("#lastName").text().charAt(0). toUpperCase();
 });
-$(window).resize(function(){
-    var cw = $('#gallery').width();
-    if(cw<=280){
 
-        $('.frame').css({'width':cw-'10px'});
-    }
+
 });
-*/
-//$(document).ready(function(){
- //   $('#galleryFoot').css({"margin-top": "300px"});
-//});
 $(document).ready(function(){
     $(".hidden").hide();
 });
@@ -58,36 +42,23 @@ $(document).ready(function(){
 var counter=0;
 
 $(".readMore").click(function(){
-    
     counter+=1;
     if($(this).text() == 'Show Less') { 
-
         if(counter%2!=0){
         $('.hidden2').slideToggle(500);
-
-       // $('#galleryFoot').css({"margin-top": "300px"});
     }
     else{
-
     $('.hidden').slideToggle(500);
     $(this).text() == 'Show Less' ? $(this).text('Show More') : $(this).text('Show Less');
-
-    //$('#galleryFoot').css({"margin-top": "300px"});
     }
-
     }
     else{
-
     if(counter%2!=0){
     $('.hidden').slideToggle(500);
-
-    //$('#galleryFoot').css({"margin-top": "300px"});
     }
     else{
-    
     $('.hidden2').slideToggle(500);
     $(this).text() == 'Show More' ? $(this).text('Show Less') : $(this).text('Show More');
-    //$('#galleryFoot').css({"margin-top": "250px"});
 }
 }
 });
